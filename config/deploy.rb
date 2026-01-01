3524d3afa4dbff3248524e431d1407b9b73793b5
Newburu <newburu@gmail.com>
2026-01-01 23:02:39 +0900

    config: Puma設定を追加しました。

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,6 +34,18 @@ set :default_env, {
   node_env: "production"
 }
 
+# Puma Configuration
+set :puma_threads,    [ 4, 16 ]
+set :puma_workers,    0
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{release_path}/log/puma.access.log"
+set :puma_error_log,  "#{release_path}/log/puma.error.log"
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, true
+
 # Default value for local_user is ENV['USER']
 # set :local_user, -> { `git config user.name`.chomp }
 
